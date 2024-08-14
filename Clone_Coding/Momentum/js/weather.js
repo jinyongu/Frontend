f45999f3ea915bca0dd8f316ddfbae2d102663d4
WooJinYong <jinyongu@naver.com>
2024-08-15 01:15:21 +0900

    add weather api

---
+++ Clone_Coding/Momentum/js/weather.js
@@ -0,0 +1,22 @@
+const API_KEY = "667b09e50484fb908c359ea4e7233462";
+
+function onGeoSuccess(position) {
+    const lat = position.coords.latitude;
+    const lon = position.coords.longitude;
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+    console.log(url);
+    fetch(url)
+    .then(response => response.json())
+    .then(data => {
+        const weather = document.querySelector("#weather span:first-child");
+        const city = document.querySelector("#weather span:last-child");
+        city.innerText = data.name;
+        weather.innerText = `${data.weather[0].main} / ${data.main.temp}°`;
+    });
+}
+
+function onGeoError() {
+    alert("Can't find you. No weather for you.")
+}
+
+navigator.geolocation.getCurrentPosition(onGeoSuccess, onGeoError);
